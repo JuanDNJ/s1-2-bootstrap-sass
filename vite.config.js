684bc0ca017bb9df2afce8c2fb094cf2587e4d01
Juan Antonio <132897973+JuanDNJ@users.noreply.github.com>
2024-02-21 09:59:12 +0100

    Configuracion base path

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import path from 'path';
 
 export default {
   root: path.resolve(__dirname, 'src'),
+  base: "https://github.com/JuanDNJ/s1-2-bootstrap-sass",
   publicDir: "../public",
   build: {
     outDir: '../dist'
