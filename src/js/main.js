9a08764dfbac0ec74244cea57f59c59f9fc4e7a3
Juan Antonio <132897973+JuanDNJ@users.noreply.github.com>
2024-02-21 10:59:16 +0100

    Anulando el envio por defecto del formulario con JS

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,10 @@ import '../scss/style.scss'
 
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
+
+
+const formContactUs = document.querySelector('.form-contact-us');
+
+formContactUs.addEventListener('submit', (event) => {
+    event.preventDefault();
+})
