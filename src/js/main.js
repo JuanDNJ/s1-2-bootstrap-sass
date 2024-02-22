4e98b4d63f6e1d5d1948e1170601b94be64e4846
Juan Antonio <juandevnjv@gmail.com>
2024-02-22 10:34:46 +0100

    modified:   src/js/main.js

--- src/js/main.js
+++ src/js/main.js
@@ -3,9 +3,3 @@ import '../scss/style.scss'
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
 
-
-const formContactUs = document.querySelector('.form-contact-us');
-
-formContactUs.addEventListener('submit', (event) => {
-    event.preventDefault();
-})
